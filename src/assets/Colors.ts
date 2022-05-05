3f548fd1e675bb93807ca0769ccaa89809f2b475
Daniel Alves <dan_alves96@hotmail.com>
2022-05-05 08:37:56 -0300

    fonts

--- src/assets/Colors.ts
+++ src/assets/Colors.ts
@@ -1,3 +1,18 @@
 export default {
-    
+    principal: '#8775FE',
+    header: '#85868A',
+    Background: 'white',
+    Background2: '#ECE9FF',
+
+    Text: 'black',
+    Text2: '#8775FE',
+    text2Dark: '#BEB5FF',
+
+    button: '#504DB6',
+    border: '#EBEBED',
+    ok: '#5A9ACF',
+    cancel: '#FD746A',
+
+    black: 'black',
+    white: 'white'
 }
